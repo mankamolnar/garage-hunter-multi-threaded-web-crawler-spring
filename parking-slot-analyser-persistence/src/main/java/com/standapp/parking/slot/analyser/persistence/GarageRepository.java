c61e87679cc7f7d03d2953b564219dd59d20a76f
MarkManoMolnar <MarkManoMolnar@ge.com>
2018-05-26 13:15:28 +0200

    Added findAllByEndDateIsNotNull() method to GrageRepository.java

--- parking-slot-analyser-persistence/src/main/java/com/standapp/parking/slot/analyser/persistence/GarageRepository.java
+++ parking-slot-analyser-persistence/src/main/java/com/standapp/parking/slot/analyser/persistence/GarageRepository.java
@@ -2,12 +2,11 @@ package com.standapp.parking.slot.analyser.persistence;
 
 import com.standapp.parking.slot.analyser.persistence.domain.DbGarage;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GarageRepository extends CrudRepository<DbGarage, Long> {
-  Optional<DbGarage> findDistinctByExternalId(Long id);
   List<DbGarage> findAllByExternalId(Long id);
+  List<DbGarage> findAllByEndDateIsNotNull();
 }
