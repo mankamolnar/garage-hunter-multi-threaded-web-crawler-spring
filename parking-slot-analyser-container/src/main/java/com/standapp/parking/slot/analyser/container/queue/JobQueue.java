1e99331f29bdf60e66d0d41da7d8be486fec90cd
MarkManoMolnar <MarkManoMolnar@ge.com>
2018-05-26 13:02:17 +0200

    In JobQueue.java changed queue implementation to ConcurrentLinkedQueue

--- parking-slot-analyser-container/src/main/java/com/standapp/parking/slot/analyser/container/queue/JobQueue.java
+++ parking-slot-analyser-container/src/main/java/com/standapp/parking/slot/analyser/container/queue/JobQueue.java
@@ -1,40 +1,30 @@
 package com.standapp.parking.slot.analyser.container.queue;
 
 import com.standapp.parking.slot.analyser.domain.queue.Job;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JobQueue {
 
-  private Map<Job, Boolean> jobs;
+  private ConcurrentLinkedQueue<Job> jobs;
 
   public JobQueue() {
-    this.jobs = new ConcurrentHashMap<>();
+    this.jobs = new ConcurrentLinkedQueue<>();
   }
 
   public void add(Job job) {
-    if (!jobs.containsKey(job)) {
-      jobs.put(job, true);
+    if (!jobs.contains(job)) {
+      jobs.add(job);
     }
   }
 
   public Optional<Job> removeFirst() {
-    Optional<Job> optionalPageUrl = getFirst();
-    optionalPageUrl.ifPresent(pageUrl -> jobs.remove(pageUrl));
-
-    return optionalPageUrl;
+    return Optional.of(jobs.poll());
   }
 
   public boolean isEmpty() {
-    return !getFirst().isPresent();
-  }
-
-  private Optional<Job> getFirst() {
-    return jobs.keySet()
-        .stream()
-        .findFirst();
+    return jobs.size() == 0;
   }
 }
