a5f62a74d22e81116061f5a11644b8f761ec010f
MarkManoMolnar <MarkManoMolnar@ge.com>
2018-06-02 17:54:31 +0200

    Finalized scheduled crawling dates in Scheduler.java

--- parking-slot-analyser-runtime/src/main/java/com/standapp/parking/slot/analyser/runtime/scheduler/Scheduler.java
+++ parking-slot-analyser-runtime/src/main/java/com/standapp/parking/slot/analyser/runtime/scheduler/Scheduler.java
@@ -4,10 +4,12 @@ import com.standapp.parking.slot.analyser.persistence.service.ModificationTimePr
 import com.standapp.parking.slot.analyser.processor.resultpage.ResultPageQueueProducer;
 import com.standapp.parking.slot.analyser.runtime.thread.ThreadPool;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class Scheduler {
 
@@ -15,10 +17,12 @@ public class Scheduler {
   private final ThreadPool threadPool;
   private final ModificationTimeProvider modificationTimeProvider;
 
-  @Scheduled(cron = "0 0/30 9,19 * * *")
+  @Scheduled(cron = "0 0/30 10,16 * * *")
   public void start() {
     modificationTimeProvider.setModificationTimeToPresentMoment();
     resultPageQueueProducer.produceResultPageUrls();
     threadPool.startThreads();
+
+    log.info("Run at " + modificationTimeProvider.getModificationTime());
   }
 }
