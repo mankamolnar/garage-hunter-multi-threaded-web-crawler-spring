cac2f54db771f86413bce0531bb0c7f082795a5e
MarkManoMolnar <MarkManoMolnar@ge.com>
2018-06-02 17:32:07 +0200

    Renamed ApplicationConfiguration to CrawlerConfiguration

--- parking-slot-analyser-runtime/src/main/java/com/standapp/parking/slot/analyser/runtime/configuration/CrawlerConfiguration.java
+++ parking-slot-analyser-runtime/src/main/java/com/standapp/parking/slot/analyser/runtime/configuration/CrawlerConfiguration.java
@@ -1,17 +1,28 @@
 package com.standapp.parking.slot.analyser.runtime.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-public class ApplicationConfiguration {
+@Slf4j
+public class CrawlerConfiguration {
+
+  @Value("${parking.slot.analyser.threads.cores}")
+  private Integer cores;
+
+  @Value("${parking.slot.analyser.threads.max-cores}")
+  private Integer maxCores;
 
   @Bean
   public ThreadPoolTaskExecutor taskExecutor() {
+    log.info("THREAD POOL EXECUTOR MAX CORES: " + cores + " " + maxCores);
+
     ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
-    pool.setCorePoolSize(5);
-    pool.setMaxPoolSize(10);
+    pool.setCorePoolSize(cores);
+    pool.setMaxPoolSize(maxCores);
     pool.setWaitForTasksToCompleteOnShutdown(true);
     pool.initialize();
 
